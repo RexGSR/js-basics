f69b09cda1bcea1053b1d7a8f29f88dbfbc1deb3
Warlock <singhgauravg40@gmail.com>
2023-05-22 22:48:12 +0530

    added naughts and croses

--- main.js
+++ main.js
@@ -1,52 +1,90 @@
-document.addEventListener('DOMContentLoaded', getLocation);
+let boxes = document.querySelectorAll('input');
+boxes = [...boxes]; // changing node list into array
+const resetBtn = document.querySelector('.reset');
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(setPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+let container  = document.querySelector('.container');
+
+container.addEventListener('click', (e) => {
+    console.log((e.target).parentNode);
+})
+
+
+let playerOne = true;
+
+boxes.map(box => {
+  box.addEventListener('click', clickHandler);
+});
+
+
+function clickHandler(event) {
+
+
+    // setting X or O based on which player's trun it is
+    playerOne ? event.target.value = 'X' : event.target.value = 'O';
+
+    // disable input
+    event.target.setAttribute('disabled', '');
+
+    // check if a current player is winning
+    let winner = checkForWinner();
+
+    if(winner)  {
+        playerOne ? alert('player one won !') : alert('player two won !');
+        resetGame();
+    } else  {
+        // change player
+        playerOne = !playerOne;
     }
+    
 }
 
-function setPosition(geoData) {
-    getWeatherData({ lat: geoData.coords.latitude, lng: geoData.coords.longitude });
-}
-const API_KEY = '2d5c6e6a6615e5a301a05071950bf2fb';
-const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather'
-
-// const getWeatherData = (locationData) => {
-//     fetch(`${WEATHER_API_URL}?lat=${locationData.lat}&lon=${locationData.lng}&appid=${API_KEY}`)
-//         .then(function (response) {
-//             (response.json())
-//             .then((data) => {
-//                 console.log(data);
-//             }).catch((error) => {
-//                 console.log(error);
-//             });
-//         }).catch( (error) => {
-//             console.log('api error ', error);
-//         } );
-     
-// }
-
-let city = document.querySelector('.city');
-let temp = document.querySelector('.temp');
-let info = document.querySelector('.info');
-// let icon = document.querySelector('.icon');
-const setData = (data) => {
-    city.textContent = data.name;
-    temp.textContent = Math.round(data.main.temp);
-    info.textContent = data.weather[0].description;
-    // icon.setAttribute('src', `${data.weather[0].icon}`)
-}
-
-const getWeatherData = async (locationData) => {
-    try {
-        let response = await fetch(`${WEATHER_API_URL}?lat=${locationData.lat}&lon=${locationData.lng}&appid=${API_KEY}&units=metric`);
-        let repsonseData = await response.json();
-        console.log(repsonseData);
-        setData(repsonseData);
-    } catch(error) {
-        console.log('api error ', error);
+function checkForWinner() {
+    // BOARD STATE OBJECT
+    let boardState = {
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+        5: '',
+        6: '',
+        7: '',
+        8: '',
+        9: ''
     }
+
+    // get boards current state
+    boxes.map( (box, index) => {
+        boardState[index + 1] = box.value;
+    });
+
+    let playerSymbolToCheck = playerOne ? 'X' : 'O';
+
+    if(boardState[1] === playerSymbolToCheck && boardState[2] === playerSymbolToCheck && boardState[3] === playerSymbolToCheck) {
+        return true
+    } else if(boardState[4] === playerSymbolToCheck && boardState[5] === playerSymbolToCheck && boardState[6] === playerSymbolToCheck) {
+        return true
+    } else if(boardState[7] === playerSymbolToCheck && boardState[8] === playerSymbolToCheck && boardState[9] === playerSymbolToCheck) {
+        return true
+    } else if(boardState[1] === playerSymbolToCheck && boardState[4] === playerSymbolToCheck && boardState[7] === playerSymbolToCheck) {
+        return true
+    } else if(boardState[2] === playerSymbolToCheck && boardState[5] === playerSymbolToCheck && boardState[8] === playerSymbolToCheck) {
+        return true
+    } else if(boardState[3] === playerSymbolToCheck && boardState[6] === playerSymbolToCheck && boardState[9] === playerSymbolToCheck) {
+        return true
+    } else if(boardState[1] === playerSymbolToCheck && boardState[5] === playerSymbolToCheck && boardState[9] === playerSymbolToCheck) {
+        return true
+    } else if(boardState[3] === playerSymbolToCheck && boardState[5] === playerSymbolToCheck && boardState[7] === playerSymbolToCheck) {
+        return true
+    } else return false;
 }
+
+function resetGame() {
+    boxes.map( (box, index) => {
+        // remove disabled input
+        box.removeAttribute('disabled');
+        box.value = '';
+    });
+    playerOne = true;
+}
+
+resetBtn.addEventListener('click', resetGame);
---
+++ old_code/weather/main.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', getLocation);
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function setPosition(geoData) {
+    getWeatherData({ lat: geoData.coords.latitude, lng: geoData.coords.longitude });
+}
+const API_KEY = '2d5c6e6a6615e5a301a05071950bf2fb';
+const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+// const getWeatherData = (locationData) => {
+//     fetch(`${WEATHER_API_URL}?lat=${locationData.lat}&lon=${locationData.lng}&appid=${API_KEY}`)
+//         .then(function (response) {
+//             (response.json())
+//             .then((data) => {
+//                 console.log(data);
+//             }).catch((error) => {
+//                 console.log(error);
+//             });
+//         }).catch( (error) => {
+//             console.log('api error ', error);
+//         } );
+     
+// }
+
+let city = document.querySelector('.city');
+let temp = document.querySelector('.temp');
+let info = document.querySelector('.info');
+// let icon = document.querySelector('.icon');
+const setData = (data) => {
+    city.textContent = data.name;
+    temp.textContent = Math.round(data.main.temp);
+    info.textContent = data.weather[0].description;
+    // icon.setAttribute('src', `${data.weather[0].icon}`)
+}
+
+const getWeatherData = async (locationData) => {
+    try {
+        let response = await fetch(`${WEATHER_API_URL}?lat=${locationData.lat}&lon=${locationData.lng}&appid=${API_KEY}&units=metric`);
+        let repsonseData = await response.json();
+        console.log(repsonseData);
+        setData(repsonseData);
+    } catch(error) {
+        console.log('api error ', error);
+    }
+}
